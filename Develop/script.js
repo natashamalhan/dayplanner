08768be50a1a564f45fe0ec9c1d3092796b2646b
Natasha Malhan <natashamalhan@Natashas-Air.attlocal.net>
2023-05-18 21:12:13 -0500

    final

--- Develop/script.js
+++ Develop/script.js
@@ -2,22 +2,26 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+$('#currentDay').text(dayjs().format('dddd, MMMM D, YYYY, h:mm'));
+$(".saveBtn").on("click", function(){
+  localStorage.setItem(this.id, $(this).siblings("textarea").val());
+})
+$(".time-block").each(function () {
+  var hour = parseInt($(this).attr("id").split("-")[1]);
+  var currentHour = dayjs().hour();
+  if (hour < currentHour) {
+    $(this).attr("class", "row time-block past");}
+    else if (hour === currentHour) {
+      $(this).attr("class", "row time-block present");}
+      else {
+        $(this).attr("class", "row time-block future");}
+        $(this).sibing("textarea").val(localStorage.getItem(this.id));
+
+})
+
+
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
 });
